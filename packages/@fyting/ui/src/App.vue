c7fefa65cebbf1945ee2707c406853388c5805d6
AkiraYasha <akirayasha@gmail.com>
2024-02-24 18:22:57 -0800

    Added initial vue project

---
+++ packages/@fyting/ui/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    Count is {{ count }}
+    <button @click="count++">+</button>
+    <button @click="count--">-</button>
+  </div>
+</template>
